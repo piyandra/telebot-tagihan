98658b3041483b5bd5813dcb30ad358277062fb8
piyan1212 <piyanstefani@gmail.com>
2024-05-18 11:25:08 +0700

    Config New

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-API_KEY = "6993598091:AAHkM6aWOzSQ4mdz7Ds91VEswbREzdPla-0"
-DB_HOST = "localhost"
-DB_USER = "root"
+API_KEY = ""
+DB_HOST = ""
+DB_USER = ""
 DB_PASSWORD = ""
-DB = "telegram_tagihan"
-SUPER_USER = 6860012691
+DB = ""
+SUPER_USER =
